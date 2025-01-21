3932e4feab79c8683351e887d486ab15f757b925
surajvns1571999 <surajvns1571999@gmail.com>
2025-01-22 00:37:43 +0530

    version2

--- src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -1,11 +1,30 @@
 package com.wellsfargo.counselor.entity;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
 public class Portfolio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
     private long portfolioId;
+
+    @Column(nullable = false)
     private String portfolioName;
+
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
 
     private long clientId;
--- src/main/java/com/wellsfargo/counselor/entity/Security.java
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -1,16 +1,41 @@
 package com.wellsfargo.counselor.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+@Entity
 public class Security {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
     private long securityId;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private LocalDateTime purchaseDate;
+
+    @Column(nullable = false)
     private double purchasePrice;
+
+    @Column(nullable = false)
     private int qualtity;
+
+    @Column(nullable = false)
     private String category;
+
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
+
 
     private long portfolioId;
 }
